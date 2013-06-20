015c1edcb6ec449a86755c9c94ac77df5f83f669
Asher King Abramson <a.kingabramson@gmail.com>
2013-06-19 21:23:36 -0700

    initial commit

---
+++ snake.js
@@ -0,0 +1,119 @@
+var SnakeGame = (function (){
+
+	function Board(height, width) {
+		this.snake = new Snake(height, width);
+		this.height = height;
+		this.width = width;
+		this.setApple();
+	}
+
+	Board.prototype.setApple = function() {
+		var applex = Math.floor(Math.random()*this.width);
+		var appley = Math.floor(Math.random()*this.height);	
+
+		this.applePos = {x: applex, y: appley};
+	}
+
+	Board.prototype.move = function() {
+		if (this.appleAt(this.snake.head)) {
+			this.setApple();
+
+		} else {
+			this.snake.body.shift();
+		}
+
+		this.snake.head = this.snake.move()
+		this.snake.body.push(this.snake.head);
+	}
+	
+	Board.prototype.appleAt = function(pos){
+		if (this.applePos.x === pos.x && this.applePos.y === pos.y) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	Board.prototype.gameOver = function() {
+		if (this.snake.head.x < 0 || this.snake.head.x >= this.width || this.snake.head.y < 0 || this.snake.head.y >= this.height) {
+			return true
+		}	else {
+			// cycle through body and check for hit
+			for (var i = 0; i < this.snake.body.length -1; i++) {
+				var pos = this.snake.body[i]
+				if (pos.x === this.snake.head.x && pos.y === this.snake.head.y) {
+					return true
+				} 
+			}
+		return false
+		}
+	}
+	
+
+
+	function Snake(bheight, bwidth) {
+		this.body = [{x:Math.floor(bwidth/2) -1 , y: Math.floor(bheight/2)}, {x: Math.floor(bwidth/2), y: Math.floor(bheight/2)}];
+		this.speed = 1;
+		this.direction = "east";
+		this.head = this.body[this.body.length - 1];
+	}
+
+	Snake.prototype.turn = function(key) {
+		switch(key) {
+		case 37:
+			this.direction = "west";
+			break;
+		case 38:
+			this.direction = "south";
+			break;
+		case 39:
+			this.direction = "east";
+			break;
+		case 40:
+			this.direction = "north";
+			break;
+		default:
+			console.log("Didn't recognize key value");
+		}
+	}
+
+
+	Snake.prototype.move = function() {
+		var newPos;
+		var head = this.head;
+
+		if (this.direction === "east") {
+			newPos = {x: head.x+1, y: head.y};
+		} else if (this.direction === "west") {
+			newPos = {x: head.x-1, y: head.y};
+		} else if (this.direction === "north") {
+			newPos = {x: head.x, y: head.y+1};
+		} else {
+			newPos = {x: head.x, y: head.y-1};
+		}
+
+		return newPos;
+	}
+
+
+
+
+
+
+	
+
+	return {
+		Snake: Snake,
+		Board: Board
+	};
+
+})();
+
+// var game = new SnakeGame.Board(100, 200);
+
+// game.snake.turn(38)
+// game.move()
+// console.log(game.snake)
+// console.log(game.applePos)
+// console.log(game.snake.ate(game))
+// console.log(game.gameOver());
---
+++ snake_UI.js
@@ -0,0 +1,85 @@
+$(function() {
+
+	var game = new SnakeGame.Board(20, 20);
+	console.log(game.snake);
+	makeBoard(game);
+	updateBoard(game);
+
+	$('html').keydown(function (event) {
+	  game.snake.turn(event.keyCode);
+	});
+
+	function runLoop() {
+		game.move();
+		updateBoard(game);
+		if (game.gameOver()) {
+			console.log("GAME OVER");
+			clearInterval(run);
+		}
+	}
+
+	// kick off run loop in 250millis
+	var time = 100;
+	var run = window.setInterval(runLoop, time);
+
+
+//event handler
+
+});
+
+function updateBoard(game) {
+	var appleSpot = game.applePos;
+	var snakeSpots = game.snake.body;
+
+	for (var i = 0; i < game.height; i++) {
+		for (var j = 0; j < game.width; j++) {
+		var cell = $("#cellid"+ i + "" + j);
+
+			if (includePos(snakeSpots, j, i)) {
+				cell.css('background-color', 'green');
+			} else if (appleSpot.y == i && appleSpot.x == j) {
+				cell.css('background-color', 'red');
+			} else {
+				cell.css('background-color', 'black');
+			}
+		}
+	}
+}
+
+function includePos(array, targetX, targetY) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].x === targetX && array[i].y === targetY) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function makeBoard(game) {
+	var boardDiv = $("<div>");
+	boardDiv.attr('id', 'board');
+	boardDiv.css('overflow', 'hidden');
+
+	for (var i = 0; i < game.height; i++) {
+		var rowDiv = $("<div>");
+		rowDiv.addClass('row');
+		rowDiv.attr("id", 'rowid'+ i);
+		rowDiv.css('overflow', 'hidden');
+
+		for (var j = 0; j < game.width; j++) {
+			var cellDiv = $("<div>");
+			cellDiv.addClass('cell');
+			cellDiv.attr('id', "cellid"+ i + "" + j);
+			cellDiv.css({'width': '25px',
+										'height': '25px',
+										'background-color': 'black',
+										'float': 'left'})
+			rowDiv.append(cellDiv);
+		}
+
+		boardDiv.append(rowDiv);
+	}
+
+	$('body').append(boardDiv);
+}
